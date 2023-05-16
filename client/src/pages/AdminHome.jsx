394523905fff4f7773bad3c6319984af6f6cb4fa
shafeeqmenoth1 <shafeeqmenoth@gmail.com>
2023-05-16 17:47:08 +0530

    redzn

--- client/src/pages/AdminHome.jsx
+++ client/src/pages/AdminHome.jsx
@@ -34,7 +34,7 @@ const handleDelete=async(id)=>{
           
 (  <tr className='bg-white border-b dark:bg-gray-800 dark:border-gray-700'>
    <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-    <img src={user.image} alt="" />
+    <img src={`http://localhost:5000/uploads/${user.image}`} alt="" />
    </td>
    <td className="px-6 py-4">{user.username}</td>
    <td className="px-6 py-4">{user.address}</td>
